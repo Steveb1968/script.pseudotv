0173d17788db1c5220a62cd45a2ddecdbccd7141
Steveb1968 <steve0@adam.com.au>
2015-10-24 14:27:13 +1030

    code cleanup

--- service.py
+++ service.py
@@ -1,6 +1,5 @@
 # modules
-from time import sleep
-import xbmc, xbmcgui
+import xbmc
 import xbmcaddon
 
 # get addon info
@@ -19,15 +18,15 @@ Enabled = __addon__.getSetting('enable')
 # start service
 def Notify():
     if (Msg == 'true'):
-        xbmc.executebuiltin("Notification( %s, %s, %d, %s)" % (__addonname__, __language__(30030), 4000, __icon__) )
+        xbmc.executebuiltin("Notification( %s, %s, %d, %s)" % (__addonname__, __language__(30030), 4000, __icon__))
         xbmc.log("AUTOSTART PTV: Notifications Enabled...")
     else:
         xbmc.log("AUTOSTART PTV: Notifications Disabled...")
 
 def autostart():
     Notify()
-    sleep(IDLE_TIME)
-    xbmc.executebuiltin("RunScript(script.pseudotv)")
+    xbmc.sleep(IDLE_TIME*1000)
+    xbmc.executebuiltin("RunScript("+__addonid__+")")
     xbmc.log("AUTOSTART PTV: Service Started...")
 
 if (Enabled == 'true'):
